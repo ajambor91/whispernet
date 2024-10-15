faa28a1a2793bf6065aa7c3ae587461dc43dbb48
aj <adam91mko@gmail.com>
2024-10-15 14:47:01 +0200

    configured project for test environment

--- ui/web/next.config.qa.mjs
+++ ui/web/next.config.qa.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://127.0.0.1:8099/api/:path*',
+                destination: 'http://gateway/api/:path*',
             },
         ];
     },

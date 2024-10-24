e436f1bc97c253e60844d527d809346ef1a1350d
aj <adam91mko@gmail.com>
2024-10-24 15:45:40 +0200

    clean code

--- signal-server/src/enums/ws-message.enum.ts
+++ signal-server/src/enums/ws-message.enum.ts
@@ -22,6 +22,8 @@ export enum WsMessageEnum {
     PeerReady = 'peer-ready',
     PeerJoin = 'peer-join',
     Listen = 'listen',
-    MsgReceived = 'msg-received'
+    MsgReceived = 'msg-received',
+    ConnectionClose = 'connection-close',
+    Retry = 'retry'
 
 }

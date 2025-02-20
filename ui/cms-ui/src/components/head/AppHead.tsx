286a869e645b726b9311471dc2fa2c61d37acbc1
aj <adam91mko@gmail.com>
2025-02-20 18:57:49 +0100

    Add Open Graph for social media
    - Add open graph image
    
    - Add Open Graph meta tags

--- ui/cms-ui/src/components/head/AppHead.tsx
+++ ui/cms-ui/src/components/head/AppHead.tsx
@@ -13,13 +13,19 @@ const AppHead: React.FC<IHeadProps> = ({lang}) => {
         <Head>
             <title>{t("title")}</title>
 
-            <meta name="keywords" content="encrypted messaging, secure chat, online privacy, end-to-end encryption, WhisperNet" />
+            <meta name="keywords"
+                  content="encrypted messaging, secure chat, online privacy, end-to-end encryption, WhisperNet"/>
 
-            <meta name="author" content="WhisperNet Team" />
+            <meta name="author" content="WhisperNet Team"/>
 
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-            <meta name="robots" content="index, follow" />
+            <meta name="robots" content="index, follow"/>
+            <meta property="og:title" content="WhisperNet"/>
+            <meta property="og:description" content="A secure and anonymous peer - to - peer encrypted chat"/>
+            <meta property="og:image" content="https://whispernet.site/o_graph_img.png"/>
+            <meta property="og:url" content="https://whispernet.site"/>
+            <meta property="og:type" content="website"/>
 
         </Head>
     )
